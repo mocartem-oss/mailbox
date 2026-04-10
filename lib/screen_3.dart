3e6f07958d489f6b52dfe4177410c843d9b2d0dc
the-kkkatya <jurchenkokatya2106@gmail.com>
2026-04-10 14:24:19 +0300

    Экран 3

---
+++ lib/screen_3.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Screen3 extends StatelessWidget {
+  const Screen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF1E1E1E),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1E1E1E),
+        leading: IconButton(
+          icon: const Icon(Icons.close),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: const Text('Новое письмо'),
+        actions: [
+          TextButton(
+            onPressed: () {
+              // Тут логика отправки
+              Navigator.pop(context);
+            },
+            child: const Text('Отправить', style: TextStyle(color: Colors.blue, fontSize: 16)),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Кому
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Кому',
+                hintStyle: const TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+              ),
+            ),
+            const Divider(color: Colors.grey),
+            // Тема
+            TextField(
+              style: const TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Тема',
+                hintStyle: const TextStyle(color: Colors.grey),
+                border: InputBorder.none,
+              ),
+            ),
+            const Divider(color: Colors.grey),
+            const SizedBox(height: 10),
+            // Текст
+            Expanded(
+              child: TextField(
+                style: const TextStyle(color: Colors.white),
+                maxLines: null, // Бесконечная высота
+                decoration: InputDecoration(
+                  hintText: 'Сообщение',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
