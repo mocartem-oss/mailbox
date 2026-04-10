ca4abe34b0eb11ed4ce431b15b6d5de6fad03cae
the-kkkatya <jurchenkokatya2106@gmail.com>
2026-04-10 14:42:26 +0300

    Экран 3

--- lib/screen_3.dart
+++ lib/screen_3.dart
@@ -6,21 +6,21 @@ class Screen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1E1E1E),
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
-        backgroundColor: const Color(0xFF1E1E1E),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
         leading: IconButton(
-          icon: const Icon(Icons.close),
+          icon: const Icon(Icons.close, color: Color.fromARGB(255, 0, 0, 0)),
           onPressed: () => Navigator.pop(context),
         ),
-        title: const Text('Новое письмо'),
+        title: const Text('Новое письмо', style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 24)),
         actions: [
           TextButton(
             onPressed: () {
               //отправка письма
               Navigator.pop(context);
             },
-            child: const Text('Отправить', style: TextStyle(color: Colors.blue, fontSize: 16)),
+            child: const Text('Отправить', style: TextStyle(color: Colors.blue, fontSize: 16, fontWeight: FontWeight.bold)),
           ),
         ],
       ),
